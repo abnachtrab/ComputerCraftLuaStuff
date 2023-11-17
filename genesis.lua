1b1a510a397a8c2e7b33c5a9e062c31908d6cb9c
Adam Nachtrab <58910398+abnachtrab@users.noreply.github.com>
2023-11-17 00:28:55 -0700

    Update genesis.lua

--- genesis.lua
+++ genesis.lua
@@ -1,36 +1,82 @@
 local minFuel = 100
+local rowLength = 6
+local numRows = 1  --- Note: One row == two crops on each side
+---   T = Turtle (with a fuel chest under it)
+---   C = Chest
+---   P = Plant (with air above it)
+---   O = Air
+---   Example with rowLength=6 and numRows=2
+---   PPPPPP
+---   OOOOOTC
+---   PPPPPP
+---   PPPPPP
+---   OOOOOO
+---   PPPPPP
+---
 print("Fuel: "..turtle.getFuelLevel().."/"..turtle.getFuelLimit())
-while true do
-    for i = 1, 16 do
-        turtle.select(i)
-        if not turtle.refuel(0) or turtle.getFuelLevel() >= turtle.getFuelLimit() then
+local hand = peripheral.find("weakAutomata")
+if not hand then
+    print("Please attach a Weak Automata Core from Advanced Peripherals!")
+else
+    while true do
+        if turtle.getFuelLevel() <= turtle.getFuelLimit() then
+            turtle.select(1)
+            local canSuck = true
+            repeat
+                canSuck = turtle.suckDown()
+                turtle.refuel()
+            until not canSuck or turtle.getFuelLevel() >= turtle.getFuelLimit()
             turtle.dropDown()
-        else
-            turtle.refuel()
-            turtle.dropDown()
         end
-    end
-    if turtle.getFuelLevel() <= turtle.getFuelLimit() then
-        turtle.select(1)
-        local canSuck = true
-        repeat
-            canSuck = turtle.suckUp()
-            turtle.refuel()
-        until not canSuck or turtle.getFuelLevel() >= turtle.getFuelLimit()
-        turtle.dropUp()
-    end
-    if turtle.getFuelLevel() < minFuel then
-        print("Out of fuel! Please place me on a chest with fuel inside!")
-        break
-    end
-    for j = 1, 8 do
-        for k = 1, j do
-            turtle.forward()
+        if turtle.getFuelLevel() < minFuel then
+            print("Out of fuel!")
+            break
         end
-        turtle.turnRight()
-        for l = 1, j do
-            turtle.forward()
+        turtle.turnLeft()
+        for i = 1, numRows do
+            for j = 1, rowLength do
+                hand.useOnBlock()
+                turtle.suck()
+                turtle.turnLeft()
+                turtle.forward()
+                turtle.turnRight()
+            end
+            turtle.turnLeft()
+            turtle.turnLeft()
+            for j = 1, rowLength do
+                hand.useOnBlock()
+                turtle.suck()
+                turtle.turnLeft()
+                turtle.forward()
+                turtle.turnRight()
+            end
+            if i < numRows then
+                turtle.up()
+                turtle.forward()
+                turtle.forward()
+                turtle.forward()
+                turtle.down()
+                turtle.turnLeft()
+                turtle.turnLeft()
+            else
+                turtle.turnLeft()
+                turtle.turnLeft()
+                turtle.up()
+                for j = 1, i*3-1 do
+                    turtle.forward()
+                end
+                turtle.down()
+                turtle.turnRight()
+            end
         end
-        turtle.turnRight()
+        for i = 1, 16 do
+            turtle.select(i)
+            if not turtle.refuel(0) or turtle.getFuelLevel() >= turtle.getFuelLimit() then
+                turtle.drop()
+            else
+                turtle.refuel()
+                turtle.drop()
+            end
+        end
     end
 end
